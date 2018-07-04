dae4f320c96f863bf660c3575511b38b32d5d306
Dan Berbec <dan393@gmail.com>
2018-07-04 11:49:39 +0100

    added new feature of fast slide show

--- tests/fi/iki/jka/JPhotoFrameTest.java
+++ tests/fi/iki/jka/JPhotoFrameTest.java
@@ -1,13 +1,33 @@
 package fi.iki.jka;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import java.awt.event.ActionEvent;
 
 public class JPhotoFrameTest {
+    int intervalTest = 0;
+
     @Test
-    public void placeholder() throws Exception {
-        assertThat(2, equalTo(2));
+    public void actionPerformed_startSlideshow() throws Exception {
+        JPhotoFrame jPhotoFrame = new JPhotoFrame(){
+           @Override
+           public void startSlideshow(int interval) {
+               // remember that startSlideshow has been called
+               intervalTest = interval;
+
+           }
+
+           @Override
+           public void setTitle() {}
+       };
+       ActionEvent actionEvent = new ActionEvent(new Object(), 0, "Start Slideshow");
+       jPhotoFrame.actionPerformed(actionEvent);
+       Assert.assertEquals(intervalTest, 5000);
+
+        actionEvent = new ActionEvent(new Object(), 0, "Start Fast Slideshow");
+        jPhotoFrame.actionPerformed(actionEvent);
+        Assert.assertEquals(intervalTest, 500);
+
     }
 }
